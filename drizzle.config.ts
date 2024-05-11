f351de2b0a244edd96042f7e4787152426e1e6c4
Adrian Zinko <zinko.adrian00@gmail.com>
2024-05-11 12:53:02 +0200

    exp: config drizzle, accounts table migrations

---
+++ drizzle.config.ts
@@ -0,0 +1,19 @@
+import { config } from "dotenv";
+import { defineConfig } from "drizzle-kit";
+
+config({ path: ".env.local" });
+
+export default defineConfig({
+  schema: "./db/schema.ts",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+  migrations: {
+    table: "migrations",
+    schema: "public",
+  },
+  out: "./db/migrations",
+  verbose: true,
+  strict: true,
+});
